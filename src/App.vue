c148344f28d561fa1e1698edbf1c4e503dbc9481
innamagkeeva <inna.m79@mail.ru>
2025-03-31 12:57:57 +0300

    сохранить изменения

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <script setup lang="ts">
-let result = ''
+// import { ref } from 'vue'
 
-function getResult(string: string): string {}
+// let result = ref('')
+
+// function getResult(){
+//   result.value +=
+// }
 </script>
 
 <template>
-  <button class="button" @click="getResult('A')">A</button>
-  <button class="button" @click="getResult('B')">B</button>
-  <button class="button" @click="getResult('C')">C</button>
-  <button class="button" @click="getResult('D')">D</button>
-  <button class="button" @click="getResult('F')">F</button>
-  <button class="button" @click="getResult('G')">G</button>
-  <button class="button" @click="getResult('H')">H</button>
-  <button class="button" @click="getResult('I')">I</button>
-  <button class="button" @click="getResult('J')">J</button>
-  <button class="button" @click="getResult('K')">K</button>
-  <button class="button" @click="getResult('L')">L</button>
-  <button class="button" @click="getResult('M')">M</button>
-  <button class="button" @click="getResult('N')">N</button>
-  <button class="button" @click="getResult('O')">O</button>
-  <button class="button" @click="getResult('P')">P</button>
-  <button class="button" @click="getResult('Q')">Q</button>
-  <button class="button" @click="getResult('R')">R</button>
-  <button class="button" @click="getResult('S')">S</button>
-  <button class="button" @click="getResult('T')">T</button>
-  <button class="button" @click="getResult('U')">U</button>
-  <button class="button" @click="getResult('V')">V</button>
-  <button class="button" @click="getResult('W')">W</button>
-  <button class="button" @click="getResult('X')">X</button>
-  <button class="button" @click="getResult('Y')">Y</button>
-  <button class="button" @click="getResult('Z')">Z</button>
-  <button class="button_space" @click="getResult('')"></button>
-  <button class="button" @click="getResult('.')">.</button>
-  <button class="button" @click="getResult(',')">,</button>
-  <button class="button" @click="getResult('!')">!</button>
-  <button class="button" @click="getResult('?')">?</button>
-  <button class="button_delete" @click="getResult('DELETE')">DELETE</button>
+  <button class="button" @click="getResult()">A</button>
+  <button class="button" @click="getResult()">B</button>
+  <button class="button" @click="getResult()">C</button>
+  <button class="button" @click="getResult()">D</button>
+  <button class="button" @click="getResult()">F</button>
+  <button class="button" @click="getResult()">G</button>
+  <button class="button" @click="getResult()">H</button>
+  <button class="button" @click="getResult()">I</button>
+  <button class="button" @click="getResult()">J</button>
+  <button class="button" @click="getResult()">K</button>
+  <button class="button" @click="getResult()">L</button>
+  <button class="button" @click="getResult()">M</button>
+  <button class="button" @click="getResult()">N</button>
+  <button class="button" @click="getResult()">O</button>
+  <button class="button" @click="getResult()">P</button>
+  <button class="button" @click="getResult()">Q</button>
+  <button class="button" @click="getResult()">R</button>
+  <button class="button" @click="getResult()">S</button>
+  <button class="button" @click="getResult()">T</button>
+  <button class="button" @click="getResult()">U</button>
+  <button class="button" @click="getResult()">V</button>
+  <button class="button" @click="getResult()">W</button>
+  <button class="button" @click="getResult()">X</button>
+  <button class="button" @click="getResult()">Y</button>
+  <button class="button" @click="getResult()">Z</button>
+  <button class="button_space" @click="getResult()"></button>
+  <button class="button" @click="getResult()">.</button>
+  <button class="button" @click="getResult()">,</button>
+  <button class="button" @click="getResult()">!</button>
+  <button class="button" @click="getResult()">?</button>
+  <button class="button_delete" @click="getResult()">DELETE</button>
   <h1 class="result">{{ result }}</h1>
 </template>
 
