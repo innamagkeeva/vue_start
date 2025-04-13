b60c63470090d1afda79f0ac83dd54fd70979696
innamagkeeva <inna.m79@mail.ru>
2025-04-14 02:46:27 +0300

    Задание 3 урока

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,122 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { computed } from 'vue'
 
-const textInput = ref('')
-const textInput2 = ref('')
+type GenderBase = {
+  value: string
+  text: string
+}
+
+const genders: GenderBase[] = ref([
+  { value: '', text: '---Выберите пол---' },
+  { value: 'male', text: 'мужской' },
+  { value: 'female', text: 'женский' },
+])
+
+const textInputName = ref('')
+const textInputSurname = ref('')
 const selectedGender = ref('')
+const isShowed = ref(false)
+
+// const buttonText = ref('показать')
+
+// function onSubmit(): void {
+//   isShowed.value = true
+//   buttonText.value = 'скрыть'
+// }
+
+function onSubmit(): void {
+  // Используем if для переключения состояния
+  if (isShowed.value) {
+    isShowed.value = false // Если данные показываются, скрываем их
+  } else {
+    isShowed.value = true // Если данные скрыты, показываем их
+  }
+}
+
+const buttonText = computed(() => (isShowed.value ? 'скрыть' : 'показать')) // Условный текст кнопки
 </script>
 
 <template>
-  <div class="user">
+  <form
+    class="form"
+    @submit.prevent="onSubmit"
+  >
     <input
-      class="user__box"
+      class="form__box"
       type="text"
       placeholder="Введите имя"
-      v-model="textInput"
+      v-model="textInputName"
     />
     <input
-      class="user__box"
+      class="form__box"
       type="text"
       placeholder="Ведите фамилию"
-      v-model="textInput2"
+      v-model="textInputSurname"
     />
-    <div class="user__box">
+    <div class="form__box">
       <select
-        class="user__select"
-        name="floor"
+        class="form__select"
+        name="gender"
         v-model="selectedGender"
       >
-        <option value="">-- Выберите пол --</option>
-        <option value="male">мужской</option>
-        <option value="female">женский</option>
+        <option
+          v-for="gender in genders"
+          :key="gender.value"
+          :value="gender.value"
+        >
+          {{ gender.text }}
+        </option>
       </select>
     </div>
     <button
-      class="user__box user__button-show"
-      type="button"
-      @click="console.log('меня нажали')"
-      :disabled="textInput.length === 0 || textInput2.length === 0 || selectedGender.length === 0"
+      class="form__box form__button-show"
+      type="submit"
+      :disabled="
+        textInputName.length === 0 || textInputSurname.length === 0 || selectedGender.length === 0
+      "
     >
-      Показать
+      {{ buttonText }}
     </button>
-  </div>
+  </form>
 
-  <div class="letter">
-    <ul class="double">
-      <li class="double__li">
-        {{ textInput }}
-      </li>
-      <li class="double__li">
-        {{ textInput2 }}
-      </li>
-      <li class="double__li double__li-3">{{ selectedGender }}</li>
+  <div
+    class="user-data"
+    v-if="isShowed"
+  >
+    <ul class="list">
+      <li class="list__li">Ваше имя: {{ textInputName }}</li>
+      <li class="list__li">Ваша фамилия: {{ textInputSurname }}</li>
+      <li class="list__li">Ваш пол: {{ selectedGender }}</li>
     </ul>
   </div>
 </template>
 
 <style>
-.user {
+.form {
   display: flex;
   flex-direction: column;
 }
 
-.double {
+.list {
   margin: 0;
   padding: 0;
+  list-style: none;
 }
 
-.user,
-.letter {
+.form,
+.user-data {
   height: 300px;
   width: 350px;
   margin: 0 auto;
 }
 
-.user__box {
+.form__box {
   border: 1px solid black;
   border-radius: 15px;
 }
-.user__box,
-.double__li {
+.form__box,
+.list__li {
   height: 40px;
   margin-bottom: 20px;
   cursor: pointer;
@@ -86,7 +125,7 @@ const selectedGender = ref('')
   outline: none;
 }
 
-.user__select {
+.form__select {
   width: 100%;
   height: 30px;
   font-size: 20px;
@@ -95,18 +134,21 @@ const selectedGender = ref('')
   cursor: pointer;
 }
 
-.user__button-show {
+.form__button-show {
   background-color: green;
 }
 
-.double {
-  list-style: none;
+.form__button-show:disabled {
+  opacity: 50%;
+  cursor: not-allowed;
 }
 </style>
 
-<!-- не понимаю как сделать чтоб вначале в инпуте был только placeholder, а при вводе - внизу в дубле шел сначала текст ("Ваше имя  или Ваша фамилия" и вводимые буквы) -->
+<!-- вопросы:
+ 1)  { value: 'male', text: 'мужской' } какую задачу имеет value: 'male' если пользователю видно только значение 'мужской'?
+ ПРАВИЛЬНО ЛИ Я ПОНИМАЮ  что это только ключ и нужен только для  работы с кодом? -->
 
-<!-- почему на кнопке задизейбливается только слово "показать", а не вся кнопка.  
- И! почему срабатывает со знаком ||  а не &&     -->
+<!-- 2)в опциях: почему в поле по умолчанию выводится строка именно с пустой строкой в value? я пробовала менять местами в коде - все равно она, значит не по порядку, а именно с пустой строкой (---Выберете пол---) -->
 
-<!-- а как открыть нижнее поле после нажатия "Показать" = вообще не догоняю... -->
+<!-- 3)
+ как при нажатии на "показать" открывать нижнее поле и менять текст на "скрыть" я быстро сама поняла, а вот обратно - вообще не придумала, хотя пыталась долго.  то что написала - взяла из GPT?, к стати несколько разных готовых вариантов посмотрела.   каждая строчка понятна, а в целом не очень складывается в голове.. надо чтоб словами ты рассказал.     ну или вариант правильнее -->
